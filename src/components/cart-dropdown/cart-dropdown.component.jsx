803bfab245e9e9a6df4770c9f81b52cb3132334d
unknown <daudakolo16@gmail.com>
2022-07-05 10:11:16 +0100

    created add to cart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,21 @@
 import "./cart-dropdown.styles.scss";
-import Button from '../button/button.component'
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import React from "react";
 
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>GO TO CHECKOUT</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button buttonType="inverted">GO TO CHECKOUT</Button>
     </div>
   );
 };
